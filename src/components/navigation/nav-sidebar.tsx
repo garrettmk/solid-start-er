932c309dac9570c87ea15587e0261d2ce66663ff
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-03 13:10:26 -0800

    new icons

--- src/components/navigation/nav-sidebar.tsx
+++ src/components/navigation/nav-sidebar.tsx
@@ -4,6 +4,8 @@ import { A } from "solid-start";
 import { useDarkMode } from "~/lib/contexts/dark-mode-context";
 import { ButtonUserMenu } from "../buttons/button-user-menu";
 import { HomeIcon } from "../icons/home-icon";
+import { BuildingStorefrontIcon } from "../icons/building-storefront-icon";
+import { UsersIcon } from "../icons/users-icon";
 
 const styles = {
   base: `
@@ -54,37 +56,13 @@ export function NavSidebar() {
         <HomeIcon />
       </A>
 
-      <a class={styles.link} href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-          />
-        </svg>
-      </a>
+      <A class={styles.link} href="/app/tenants">
+        <BuildingStorefrontIcon />
+      </A>
 
-      <a class={styles.link} href="#">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-          />
-        </svg>
-      </a>
+      <A class={styles.link} href="/app/users">
+        <UsersIcon />
+      </A>
 
       <hr />
 
