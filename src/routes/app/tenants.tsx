d6d2285d3127449e43de6554138698568b9586a9
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-03 17:38:45 -0800

    added layout for tenants module

---
+++ src/routes/app/tenants.tsx
@@ -0,0 +1,33 @@
+import { Outlet } from "solid-start";
+import { Button } from "~/components/buttons/button";
+import { BuildingStorefrontIcon } from "~/components/icons/building-storefront-icon";
+import { ModuleSidebar } from "~/components/module/module-sidebar";
+import { NavMenu } from "~/components/navigation/nav-menu";
+import { NavMenuHeader } from "~/components/navigation/nav-menu";
+import { NavMenuItem } from "~/components/navigation/nav-menu";
+import { PageHeader } from "~/components/page/page-header";
+
+export function TenantsLayout() {
+  return (
+    <>
+      <ModuleSidebar>
+        <PageHeader class="-mx-5 -my-3 mb-6">
+          <h1 class="text-lg text-slate-800 dark:text-slate-200">Tenants</h1>
+        </PageHeader>
+        <NavMenu>
+          <Button size="xs" class="w-full mb-6">
+            <BuildingStorefrontIcon size="xs" class="mr-2" />
+            New Tenant
+          </Button>
+          <NavMenuHeader>Manage</NavMenuHeader>
+          <NavMenuItem active>Tenants</NavMenuItem>
+        </NavMenu>
+      </ModuleSidebar>
+      <div class="ml-64">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default TenantsLayout;
