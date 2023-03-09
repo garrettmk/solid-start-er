370d72448a4a778f57a799e3327fffbf4f1b2714
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-09 00:38:37 -0800

    added position prop

--- src/components/overlays/blur-overlay.tsx
+++ src/components/overlays/blur-overlay.tsx
@@ -3,18 +3,21 @@ import { JSX, splitProps } from "solid-js";
 
 export interface BlurOverlayProps extends JSX.HTMLAttributes<HTMLDivElement> {
   isOpen?: boolean;
+  position?: "fixed" | "absolute";
 }
 
 export function BlurOverlay(props: BlurOverlayProps) {
-  const [, divProps] = splitProps(props, ["class", "isOpen"]);
+  const [, divProps] = splitProps(props, ["class", "isOpen", "position"]);
 
   return (
     <div
       class={clsx(
-        "fixed inset-0 z-[10000] overflow-x-hidden overflow-y-auto backdrop-blur-sm",
+        props.position === "absolute" ? "absolute" : "fixed",
+        "fixed inset-0 overflow-x-hidden overflow-y-auto backdrop-blur-sm",
         {
           hidden: !props.isOpen,
-        }
+        },
+        props.class
       )}
       {...divProps}
     />
