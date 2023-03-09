b3d0518fb9aeee215fffeffd9d43c785991d294d
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-09 00:42:40 -0800

    added is_super_admin utility function

--- supabase/sql/utils.sql
+++ supabase/sql/utils.sql
@@ -1,5 +1,5 @@
 -- Active: 1677982949012@@127.0.0.1@54322@postgres
-CREATE OR REPLACE FUNCTION get_initials(name TEXT) RETURNS TEXT
+CREATE OR REPLACE FUNCTION public.get_initials(name TEXT) RETURNS TEXT
 AS $$
   DECLARE
       initials TEXT := '';
@@ -11,3 +11,14 @@ AS $$
       RETURN initials;
   END;
 $$ LANGUAGE plpgsql;
+
+
+create or replace function public.is_super_admin() returns boolean
+as $$
+    declare
+        result boolean;
+    begin
+        select is_super_admin into result from auth.users where id = auth.uid();
+        return result;
+    end;
+$$ language plpgsql security definer;
