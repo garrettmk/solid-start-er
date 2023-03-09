273981a40e416047ef4df67c5fa936140cf442be
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-09 00:43:36 -0800

    bug fixes

--- supabase/sql/02-application/01-application-roles.sql
+++ supabase/sql/02-application/01-application-roles.sql
@@ -17,7 +17,7 @@ create table public.application_roles (
 
 -- Role permissions for various subjects
 CREATE TABLE public.application_role_permissions (
-    role_id REFERENCES public.application_roles NOT NULL,
+    role_id bigint REFERENCES public.application_roles (id) NOT NULL,
     subject TEXT NOT NULL,
     action TEXT NOT NULL,
     PRIMARY KEY (role_id, subject, action)
@@ -26,8 +26,8 @@ CREATE TABLE public.application_role_permissions (
 -- Assignments of roles to users
 CREATE TABLE public.application_users (
     user_id UUID REFERENCES auth.users NOT NULL,
-    role_id BIGINT REFERENCES public.application_roles NOT NULL,
-    PRIMARY KEY (user_id, role)
+    role_id BIGINT REFERENCES public.application_roles (id) NOT NULL,
+    PRIMARY KEY (user_id, role_id)
 );
 
 
@@ -82,64 +82,71 @@ alter table public.application_roles enable row level security;
 alter table public.application_role_permissions enable row level security;
 alter table public.application_users enable row level security;
 
-create or replace policy "Only superusers can create application roles" on public.application_roles
+create policy "Only superusers can create application roles" on public.application_roles
   for insert with check (public.is_super_admin());
 
-create or replace policy "Any user can read the application roles" on public.application_roles
+create policy "Any user can read the application roles" on public.application_roles
   for select using (true);
 
-create or replace policy "Users with the 'update' permission can update application roles" on public.application_roles
+create policy "Users with the 'update' permission can update application roles" on public.application_roles
   for update using (
     public.is_super_admin() OR
     public.has_application_permissions('application_roles', 'update')
   );
 
-create or replace policy "Only superusers can delete application roles" on pulic.application_roles
+create policy "Only superusers can delete application roles" on public.application_roles
   for delete using (public.is_super_admin());
 
 
-create or replace policy "Only superusers can add role permissions" on public.application_role_permissions
+create policy "Only superusers can add role permissions" on public.application_role_permissions
   for insert with check (public.is_super_admin());
 
-create or replace policy "Role permissions can't be modified" on public.application_role_permissions
+create policy "Role permissions can't be modified" on public.application_role_permissions
   for update using (false);
 
-create or replace policy "Users can only read the permissions of roles they belong to" on public.application_role_permissions
+create policy "Users can only read the permissions of roles they belong to" on public.application_role_permissions
   for select using (
     public.is_super_admin() or
     public.has_application_role(role_id)
   );
 
-create or replace policy "Only superusers can delete role permissions" on public.application_role_permissions
+create policy "Only superusers can delete role permissions" on public.application_role_permissions
   for delete using (public.is_super_admin());
 
 
-create or replace policy "Only users with the 'create' permission can assign application roles" on public.application_users
+create policy "Only users with the 'create' permission can assign application roles" on public.application_users
   for insert with check (
     public.is_super_admin() or
     public.has_application_permissions('application_users', 'create')
   );
 
-create or replace policy "Any user can read application role assignments" on public.application_users
+create policy "Any user can read application role assignments" on public.application_users
   for select using (true);
 
-create or replace policy "Application role assignments can't be modified" on public.application_users
+create policy "Application role assignments can't be modified" on public.application_users
   for update using (false);
 
-create or replace policy "Only users with the 'delete' permission can remove application role assignments" on public.application_users
+create policy "Only users with the 'delete' permission can remove application role assignments" on public.application_users
   for delete using (
     public.is_super_admin() or
     public.has_application_permissions('application_users', 'delete')
   );
 
 
+/*
+  Views
+*/
 
-
-
-
-
-
-
-
-ALTER TABLE public.application_roles ENABLE ROW LEVEL SECURITY;
-
+create view public.application_role_assignments as
+  select 
+    au.user_id, 
+    au.role_id, 
+    p.full_name, 
+    ar.name, 
+    ar.description 
+  from 
+    public.application_users au 
+  left join public.user_profiles p 
+    on p.id = au.user_id 
+  left join public.application_roles ar 
+    on au.role_id = ar.id;
