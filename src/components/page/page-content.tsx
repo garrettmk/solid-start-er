3bcddea7e2826f2c4bfbd498911759e82572f3b8
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-09 00:40:09 -0800

    now takes dynamic component prop

--- src/components/page/page-content.tsx
+++ src/components/page/page-content.tsx
@@ -1,14 +1,18 @@
 import clsx from "clsx";
-import { JSX, splitProps } from "solid-js";
+import { JSX, splitProps, ValidComponent } from "solid-js";
+import { Dynamic } from "solid-js/web";
 
-export interface PageContentProps extends JSX.HTMLAttributes<HTMLDivElement> {}
+export interface PageContentProps extends JSX.HTMLAttributes<HTMLDivElement> {
+  component?: ValidComponent;
+}
 
 export function PageContent(props: PageContentProps) {
-  const [, divProps] = splitProps(props, ["class"]);
+  const [, divProps] = splitProps(props, ["class", "component"]);
 
   return (
     // @ts-ignore
-    <main
+    <Dynamic
+      component={props.component ?? "main"}
       class={clsx("p-12 text-slate-800 dark:text-slate-200", props.class)}
       {...divProps}
     />
