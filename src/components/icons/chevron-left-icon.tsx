74294fb375a3590413a83d51d2f989e6a9ac6337
Garrett Myrick <garrettmyrick@gmail.com>
2023-03-13 19:19:11 -0700

    fixed icon path

--- src/components/icons/chevron-left-icon.tsx
+++ src/components/icons/chevron-left-icon.tsx
@@ -4,9 +4,9 @@ export function ChevronLeftIcon(props: IconProps) {
   return (
     <Icon {...props}>
       <path
-        fill-rule="evenodd"
-        d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"
-        clip-rule="evenodd"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15.75 19.5L8.25 12l7.5-7.5"
       />
     </Icon>
   );
